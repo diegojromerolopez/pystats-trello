36406b4475d1dcbfec65228ee2a740b1da1240aa
diegojromerolopez <diegojromerolopez@gmail.com>
2016-04-15 21:50:05 +0200

    Generation of all stats and charts for a Trello board

---
+++ printer/printer.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from slugify import slugify
+import datetime
+import settings
+
+
+# Small utility class that prints in stdio and
+class Printer(object):
+
+    def __init__(self, filename, print_in_stdio=True):
+        self.filename = filename
+        self.string = u""
+        self.print_in_stdio = print_in_stdio
+
+    def p(self, text):
+        if self.print_in_stdio:
+            print(text)
+        self.string += text + u"\n"
+
+    def newline(self):
+        self.p(u"\n")
+
+    def flush(self):
+        now_str = datetime.datetime.now(settings.TIMEZONE).strftime("%Y_%m_%d_%H_%M_%S")
+        output_filename = u"{0} {1}.txt".format(slugify(self.filename), now_str)
+        with open(output_filename, "w") as output_file:
+            output_file.write(self.string.encode('utf8'))
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import settings
 from charts import trellochart
+from printer.printer import Printer
 from stats import trellostats
 from connector import connect
 
@@ -14,42 +15,51 @@ if __name__ == "__main__":
 
     stats = trellostats.get_stats(client, board_name=settings.TEST_BOARD)
 
-    print("-------------------------------------------------------")
-    print(u"Measurements for {0}".format(settings.TEST_BOARD))
+    printer = Printer(u"results_for_{0}_board".format(settings.TEST_BOARD))
+
+    printer.newline()
+    printer.p(u"Measurements for {0}".format(settings.TEST_BOARD))
 
     # Average time in each column for all the cards
-    print(u"# Average time in each column for all the board cards")
+    printer.p(u"# Average time in each column for all the board cards")
     for list_ in stats["lists"]:
         list_id = list_.id
         list_name = list_.name.decode("utf-8")
         avg_list_time = stats["time_by_list"][list_id]["avg"]
         std_dev_list_time = stats["time_by_list"][list_id]["std_dev"]
-        print(u"- {0}: {1} h (std. dev. {2})".format(list_name, avg_list_time, std_dev_list_time))
+        printer.p(u"- {0}: {1} h (std. dev. {2})".format(list_name, avg_list_time, std_dev_list_time))
 
-    # Average forward/backward movements by column for all the cards
-    print(u"# Average time in each column for all the board cards")
+    printer.newline()
+
+    # Forward/backward movements by column for all the cards
+    printer.p(u"# Sum of forward/backward movements by source column for all the cards")
     for list_ in stats["lists"]:
         list_id = list_.id
         list_name = list_.name.decode("utf-8")
-        forward_movements = stats["backward_movements_by_list"][list_id]
+        forward_movements = stats["forward_movements_by_list"][list_id]
         backward_movements = stats["backward_movements_by_list"][list_id]
-        print(u"- {0}".format(list_name))
-        print(u"  - Forward movs: {0}".format(forward_movements))
-        print(u"  - Backward movs: {0}".format(backward_movements))
+        printer.p(u"- {0}".format(list_name))
+        printer.p(u"  - Forward movements: {0}".format(forward_movements))
+        printer.p(u"  - Backward movements: {0}".format(backward_movements))
 
-    print("-------------------------------------------------------")
+    printer.newline()
 
     # Cycle time
-    print(u"# Cycle")
-    print(u"- avg: {0} h, std_dev: {1}".format(stats["cycle_time"]["avg"], stats["cycle_time"]["std_dev"]))
+    printer.p(u"# Cycle")
+    printer.p(u"Time between development state and reaching 'Done' state.")
+    printer.p(u"- avg: {0} h, std_dev: {1}".format(stats["cycle_time"]["avg"], stats["cycle_time"]["std_dev"]))
 
-    print(u"# Lead")
-    print(u"- avg: {0} h, std_dev: {1}".format(stats["lead_time"]["avg"], stats["lead_time"]["std_dev"]))
+    # Lead time
+    printer.p(u"# Lead")
+    printer.p(u"Time from start to end ('Done' state).")
+    printer.p(u"- avg: {0} h, std_dev: {1}".format(stats["lead_time"]["avg"], stats["lead_time"]["std_dev"]))
 
     # Chart with times for all cards in each column
     file_paths = trellochart.get_graphics(stats, board_name=settings.TEST_BOARD)
 
-    print("-------------------------------------------------------")
-    print(u"Chart with average card time in each list created in {0}".format(file_paths["time"]))
-    print(u"Chart with average time a list is a forward destination in {0}".format(file_paths["forward"]))
-    print(u"Chart with average time a list is a backward destination in {0}".format(file_paths["backward"]))
+    printer.newline()
+    printer.p(u"Chart with average card time in each list created in {0}".format(file_paths["time"]))
+    printer.p(u"Chart with average time a list is a forward destination in {0}".format(file_paths["forward"]))
+    printer.p(u"Chart with average time a list is a backward destination in {0}".format(file_paths["backward"]))
+
+    printer.flush()
